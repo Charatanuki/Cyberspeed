d5c09b095e962cb45a81eb07bb73499fa985aefb
DESKTOP-O42H94M\kiryl <valadzko.kiryl@gmail.com>
2023-04-16 13:47:36 -0400

    Cam Modification

--- Assets/Script/CamSwitch.cs
+++ Assets/Script/CamSwitch.cs
@@ -7,17 +7,56 @@ public class CamSwitch : MonoBehaviour
 
     public GameObject Camera1;
     public GameObject Camera2;
+    public GameObject Camera3;
+    public GameObject Camera4;
+    public GameObject Camera5;
+    public GameObject Person;
 
     void Update() {
-        if  (Input.GetButtonDown("9Key")) 
+        if  (Input.GetButtonDown("1Key")) 
        {
            Camera1.SetActive(true);
            Camera2.SetActive(false);
+           Camera3.SetActive(false);
+           Camera4.SetActive(false);
+           Camera5.SetActive(false);
+           Person.SetActive(true);
        }
-        if  (Input.GetButtonDown("0Key")) 
+        if  (Input.GetButtonDown("RKey")) 
        {
            Camera1.SetActive(false);
            Camera2.SetActive(true);
+           Camera3.SetActive(false);
+           Camera4.SetActive(false);
+           Camera5.SetActive(false);
+           Person.SetActive(true);
+       }
+       if  (Input.GetButtonDown("9Key")) 
+       {
+           Camera1.SetActive(false);
+           Camera2.SetActive(false);
+           Camera3.SetActive(true);
+           Camera4.SetActive(false);
+           Camera5.SetActive(false);
+           Person.SetActive(true);
+       }
+       if  (Input.GetButtonDown("0Key")) 
+       {
+           Camera1.SetActive(false);
+           Camera2.SetActive(false);
+           Camera3.SetActive(false);
+           Camera4.SetActive(true);
+           Camera5.SetActive(false);
+           Person.SetActive(true);
+       }
+       if  (Input.GetButtonDown("-Key")) 
+       {
+           Camera1.SetActive(false);
+           Camera2.SetActive(false);
+           Camera3.SetActive(false);
+           Camera4.SetActive(false);
+           Camera5.SetActive(true);
+           Person.SetActive(false);
        }
     }
 }
